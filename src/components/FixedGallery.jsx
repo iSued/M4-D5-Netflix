aefcee5f6bd2d9c43a7da027cc78174a222a226c
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2020-11-20 13:58:43 +0000

    added functioning searchbar

---
+++ src/components/FixedGallery.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import SingleCarousel from "./SingleCarousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import OwlCarousel from "react-owl-carousel";
+import "./DynamicGallery.css";
+class FixedGallery extends React.Component {
+  state = {
+    movieArray: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        `http://www.omdbapi.com/?apikey=1846c79&s=${this.props.searchQuery}`
+      );
+      let paresdResponse = await response.json();
+      let movieArray1 = paresdResponse.Search;
+      response = await fetch(
+        `http://www.omdbapi.com/?apikey=1846c79&s=${this.props.searchQuery}&page=2`
+      );
+      paresdResponse = await response.json();
+      let movieArray2 = paresdResponse.Search;
+      let totalArray = movieArray1.concat(movieArray2);
+      this.setState({ movieArray: totalArray });
+      console.log(this.state.movieArray);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  render() {
+    console.log(this.state.movieArray, "here");
+    return (
+      <>
+        <h2 className="align-self-start">
+          Search for: {this.props.searchQuery}
+        </h2>
+
+        {this.state.movieArray.length > 0 && (
+          <OwlCarousel margin={10}>
+            {this.state.movieArray.map((movie) => (
+              <div className="item" style={{ width: "160px" }}>
+                <img src={movie.Poster} alt="" />
+                <div></div>
+              </div>
+            ))}
+          </OwlCarousel>
+        )}
+      </>
+    );
+  }
+}
+
+export default FixedGallery;
